f721cb19a3e208fd657a8550a89ca7f7913736a8
Alexander Koc <alkc@users.noreply.github.com>
2023-05-24 12:37:01 +0200

    Add explanations to code

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,9 +6,12 @@ from app import create_app
 
 @pytest.fixture()
 def app(mocker, mock_database_client):  # noqa: F811
-    """Return empty app"""
-
+    """Return app connected to mock database"""  
     mock_mongo = mocker.patch("app.mongo._get_mongo_client")
+    # the internal _get_mongo_client function is modified below to return the pre-configured
+    # mongomock.MongoClient, instead of trying to initialize and return a pymongo.MongoClient
+    # the MongoDB object will be initialized with the mongomock.MongoClient, when create_app
+    # is called below.
     mock_mongo.return_value = mock_database_client
 
     app = create_app()
@@ -31,8 +34,14 @@ def database_item():
 
 @pytest.fixture()
 def mock_database_client(database_item):
-    """Populated mock db"""
+    """
+    Populated mock db
+    
+    This mock client will repleace the real pymongo client
+    when running the tests.
+    """
     client = mongomock.MongoClient()
+    # Insert one example object:
     client["foo"]["bar"].insert_one(database_item)
 
     return client
