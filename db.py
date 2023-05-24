58ec400218d8a7aef4b52ebd0b1c6eaf2107382a
Alexander Koc <alexander.koc@skane.se>
2023-05-24 11:52:19 +0200

    Add internal function for returning a mongoclient based on mongo_uri
    The return value of this function can be edited using mocker to return a pre-configured mongomock.MongoClient instead

--- db.py
+++ db.py
@@ -1,7 +1,11 @@
 import pymongo
 
 
-class FooHandler:
+class ItemHandler:
+
+    """
+    Mixin for dealing with items from a specific foo.bar database.
+    """
 
     bar_collection: pymongo.collection.Collection
 
@@ -9,12 +13,21 @@ class FooHandler:
         return self.bar_collection.find_one()
 
 
-class MongoDB(FooHandler):
+class MongoDB(ItemHandler):
     def __init__(self):
         self.client = None
 
     def init_from_app(self, app):
-
+        """
+        Set up a mongoclient using app.config
+        """
         mongo_uri = app.config["MONGO_URI"]
-        self.client = pymongo.MongoClient(mongo_uri)
+        self.client = self._get_mongo_client(mongo_uri)
         self.bar_collection = self.client["foo"]["bar"]
+
+    def _get_mongo_client(self, mongo_uri):
+        """
+        This function will be mocked in testing and modified
+        to return a mongomock.MongoClient instead.
+        """
+        return pymongo.MongoClient(mongo_uri)
