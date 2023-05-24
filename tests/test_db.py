a3e637f0065c4417ecf0ff8aa56c996248744f58
Alexander Koc <alkc@users.noreply.github.com>
2023-05-24 12:31:29 +0200

    Simplify tests

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,12 +1,6 @@
-def test_db_views_1(app):
+def test_db_view(app):
     with app.test_client() as client:
-        response = client.get("/foo1")
+        response = client.get("/foo")
         assert response.status_code == 200
         assert b"foo" in response.data
 
-
-def test_db_views_2(app):
-    with app.test_client() as client:
-        response = client.get("/foo2")
-        assert response.status_code == 200
-        assert b"foo" in response.data
